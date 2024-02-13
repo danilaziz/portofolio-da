435e14c863d5ae1fa0fcf7134aa32a988871d87b
danilaziz <danielazizz52@gmail.com>
2024-02-13 08:07:34 +0800

    menambahkan class active menggunakan javascript di navbar

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,16 @@ window.onbeforeunload = () => {
     form.reset();
   }
 };
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myNav");
+var btns = header.getElementsByClassName("btns");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
