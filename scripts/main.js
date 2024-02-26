19e4c3db1c85c21a5379262dfc0ca7e92ed1da30
danilaziz <danielazizz52@gmail.com>
2024-02-26 12:22:11 +0800

    menambahkan kelas aktif pada navbar menggunakan javascript

--- scripts/main.js
+++ scripts/main.js
@@ -11,22 +11,43 @@ const navSlide = () => {
 
 navSlide();
 
-// Clear form before unload
+// Membersihkan form contact
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
     form.reset();
   }
 };
 
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myNav");
-var btns = header.getElementsByClassName("btns");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
+// // Add active class to the current button (highlight it)
+// var header = document.getElementById("myNav");
+// var btns = header.getElementsByClassName("btns");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     if (current.length > 0) {
+//       current[0].className = current[0].className.replace(" active", "");
+//     }
+//     this.className += " active";
+//   });
+// }
+
+// Navbar Kelas Aktif
+
+let sections = document.querySelectorAll("section");
+let NavLinks = document.querySelectorAll("nav ul li a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      NavLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("nav ul li a[href*=" + id + "]").classList.add("active");
+      });
     }
-    this.className += " active";
   });
-}
+};
